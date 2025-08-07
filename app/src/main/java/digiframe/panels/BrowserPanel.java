4bf523a5c78c1342f22799879478b82736dec779
phoenixcopp <phoenixcopp@yahoo.com>
2025-08-07 15:43:39 -0700

    javafx working

---
+++ app/src/main/java/digiframe/panels/BrowserPanel.java
@@ -0,0 +1,9 @@
+package digiframe.panels;
+
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javax.swing.JPanel;
+
+public class BrowserPanel extends JPanel {
+    WebEngine webEngine;
+}
